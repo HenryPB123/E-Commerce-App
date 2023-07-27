6b32aee686b00485060d571964ace6ee34ff76ed
Henry P B <mahe2di@gmail.com>
2023-07-26 23:42:45 -0500

    Products and Categoties

--- Client/src/data.js
+++ Client/src/data.js
@@ -25,17 +25,52 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "a",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMqN1Zzq5spdFqEjXF4accAe2jhwkUaD2V71woEU0wNLQ9extK_CdKrL7z-RMWv9dvAMA&usqp=CAU",
     title: "SHIRT STYLE",
   },
   {
     id: 2,
-    img: "s",
+    img: "https://media1.popsugar-assets.com/files/thumbor/2jhlq3u_u4zFKMePDZhsOZxvYCk/0x190:1500x1690/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/03/24/815/n/1922564/03e90c5e5e7a52810c9cf8.43185209_/i/best-loungewear-from-free-people.jpg",
     title: "LOUNGEWEAR LOVE",
   },
   {
     id: 3,
-    img: "d",
+    img: "https://image-resizing.booztcdn.com/lee-jeans/ljsl55few30_ccharcoal_v30_10.webp?has_grey=0&has_webp=0&size=w400&dpr=2.5",
     title: "LIHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://www.stoneycreekhunting.co.nz/image/cache/catalog/product_images/corporate/mens/shirts/Mens_Corporate_Shirt_Long_Sleeve_Float_Navy-875x1000.jpg",
+  },
+  {
+    id: 2,
+    img: "https://contents.mediadecathlon.com/p1786958/k$2b0a8a97ea3b1154f2f3734009451fe2/pantalon-de-montana-y-trekking-viaje-de-hombre-forclaz-travel-100-gris.jpg?format=auto&quality=40&f=800x800",
+  },
+  {
+    id: 3,
+    img: "https://http2.mlstatic.com/D_NQ_NP_704463-MLV48875001861_012022-V.jpg",
+  },
+  {
+    id: 4,
+    img: "https://lorens.com.co/wp-content/uploads/2020/11/1481.jpg",
+  },
+  {
+    id: 5,
+    img: "https://cf.shopee.com.co/file/1517f05ca5dd40b8beeb906d10aa4756",
+  },
+  {
+    id: 6,
+    img: "https://i.linio.com/p/fc11981f6f94ac475799c4ff3d4a5795-product.jpg",
+  },
+  {
+    id: 7,
+    img: "https://ae01.alicdn.com/kf/S91fe4c4135bd4403978235c7ee02417bU/Blusa-informal-de-gasa-para-mujer-camisa-de-retazos-de-encaje-con-volantes-Color-puro-cuello.jpg",
+  },
+  {
+    id: 8,
+    img: "https://m.media-amazon.com/images/I/61nr7AOz2BL._AC_SY535_.jpg",
   },
 ];
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Announcements from "../components/Announcements";
 import Categories from "../components/Categories";
 import NavBar from "../components/NavBar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 const Home = () => {
@@ -10,6 +12,8 @@ const Home = () => {
       <NavBar />
       <Slider />
       <Categories />
+      <Products />
+      <Newsletter />
     </div>
   );
 };
