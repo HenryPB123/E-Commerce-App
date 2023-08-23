a1d520edb0c1cbd24b8617bff30f1f6d7a4cfb20
Henry P B <mahe2di@gmail.com>
2023-08-22 23:59:37 -0500

    creating routes user and product

--- Api/index.js
+++ Api/index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 //Routes
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 const app = express();
 dotenv.config(); //Allows you  read variables that come from .env file
@@ -20,6 +22,8 @@ app.use(express.json());
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server running");
