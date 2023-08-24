8f464126701518b0603b31412471102261b81e3a
Henry P B <mahe2di@gmail.com>
2023-08-23 23:20:56 -0500

    more routes back

--- Api/index.js
+++ Api/index.js
@@ -6,6 +6,7 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 const app = express();
 dotenv.config(); //Allows you  read variables that come from .env file
@@ -23,7 +24,8 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
-app.use("/api/cart", cartRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server running");
