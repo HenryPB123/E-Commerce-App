3cdba1527160463ff150c193402bee49df0efbe1
Henry P B <mahe2di@gmail.com>
2023-08-24 23:34:14 -0500

    payments back

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 //Routes
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
@@ -19,6 +20,7 @@ mongoose
   .then(() => console.log("DB Connection successfull!!"))
   .catch((error) => console.log(error));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoute);
